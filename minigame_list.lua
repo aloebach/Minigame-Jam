dc222d8b17ceb64d3b80b2b8edfbd923d745ce50
aloebach <90380755+aloebach@users.noreply.github.com>
2022-03-27 22:35:44 +0200

    removing walz from minigame list

--- minigame_list.lua
+++ minigame_list.lua
@@ -1,9 +1,12 @@
 minigame_list = {
+	--'Retro_ref',
 	'bird_hunt',
 	'monster_mash',
 	'hello_world',
+	--'Problem_Trolley',
+	--'tetwist',
 	'TV_Tuner',
 	'dusty_cartridge',
-	'a_sketchy_etcher',
-	'waltz',
+	'a_sketchy_etcher'
+	--'minigame_template',
 }
